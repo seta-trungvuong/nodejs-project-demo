b44b653e1e79cf08807273df9176ea4ee9a10584
seta-trungvuong <tvuong@veritone.com>
2024-01-13 22:24:46 +0700

    update test

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -5,6 +5,6 @@ import app from "../src/app";
 describe("Test app.ts", () => {
   test("Catch-all route", async () => {
     const res = await request(app).get("/");
-    expect(res.body).toEqual({ message: "Allo! Catch-all route." });
+    expect(res.body).toEqual({ message: "Hello. This is a NodeJs Project" });
   });
 });
