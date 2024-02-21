0fe416aaee37b92833a2b9826782992046932002
seta-trungvuong <tvuong@veritone.com>
2024-02-21 14:34:24 +0700

    update init message

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use("/users", userRoutes);
 
 app.use("/", (req: Request, res: Response, next: NextFunction): void => {
-  res.json({ message: "Hello. This is a NodeJs Project" });
+  res.json({ message: "Hello. This is a NodeJs Project updated 38b4b828-0fb9-428a-8d0b-8770184607aa" });
 });
 
 export default app;
