993920eab5f10ba06dcd34b6b60fbc63d84051d9
seta-trungvuong <tvuong@veritone.com>
2024-02-21 14:37:18 +0700

    update test

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -5,6 +5,6 @@ import app from "../src/app";
 describe("Test app.ts", () => {
   test("Catch-all route", async () => {
     const res = await request(app).get("/");
-    expect(res.body).toEqual({ message: "Hello. This is a NodeJs Project" });
+    expect(res.body).toEqual({ message: "Hello. This is a NodeJs Project updated 38b4b828-0fb9-428a-8d0b-8770184607aa" });
   });
 });
